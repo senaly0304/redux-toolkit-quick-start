c94599902c215a04e76fc06f678be6baadaaee9c
senaly0304 <senaly.0304@gmail.com>
2024-07-19 17:37:49 +0900

    Done

---
+++ app/components/StoreProvider.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { ReactNode } from "react"
+import { Provider } from "react-redux";
+import { store } from "../store";
+
+type StoreProviderProps = {
+  children: ReactNode;
+};
+
+export default function StoreProvider({
+  children,
+}: Readonly<StoreProviderProps>) {
+  return (
+    <>
+      <Provider store={store}>
+        {children}
+      </Provider>
+    </>
+  );
+}
